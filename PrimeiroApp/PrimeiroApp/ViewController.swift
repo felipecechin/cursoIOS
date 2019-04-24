3abaad9425a14f215a075745db3890bb196f658b
Felipe Cechin <ficechin@hotmail.com>
2019-04-24 16:13:36 -0300

    Primeiro App iOS.

---
+++ PrimeiroApp/PrimeiroApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PrimeiroApp
+//
+//  Created by Felipe on 24/04/2019.
+//  Copyright © 2019 Felipe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //exibir uma mensagem
+        print("meu primeiro app")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
